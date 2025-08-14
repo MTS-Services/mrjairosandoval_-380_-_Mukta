787f3d2d6b4954512f78b8cd7a44d2509ee79785
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-08-14 12:31:23 +0600

    update

--- app/Models/Services.php
+++ app/Models/Services.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\BaseModel;
 
-class Services extends Model
+class Services extends BaseModel
 {
     use SoftDeletes;
     protected $table = 'services';
